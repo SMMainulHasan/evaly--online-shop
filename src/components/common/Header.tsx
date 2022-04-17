8c61d2da002866f8f09e526c089c7edc12c661f1
SMMainulHasan <mainulhasan7070@gmail.com>
2022-04-17 17:55:56 +0600

    feat: added top header

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,8 +1,29 @@
 import { Container, Nav, Navbar } from "react-bootstrap";
+import { BiPhoneCall } from "react-icons/bi";
+import { FiMail } from "react-icons/fi";
 
 function Header() {
   return (
-    <div>
+    <div className="header__component">
+      <div className="top-header py-1 bg-light border-bottom">
+        <Container className="d-flex align-items-center justify-content-between">
+          <ul className="list-unstyled d-flex gap-4">
+            <li>
+              <BiPhoneCall />
+              <a href="tel: o1701330261">o1701330261</a>
+            </li>
+            <li>
+              <FiMail />
+              <a href="mail-to: support@evaly.com">support@evaly.com</a>
+            </li>
+          </ul>
+          <div>
+            <a href="https://play.google.com/store/apps/details?id=bd.com.fvaly.fvalyshop">
+              Save big on our app!
+            </a>
+          </div>
+        </Container>
+      </div>
       <Navbar bg="dark" variant="dark">
         <Container>
           <Navbar.Brand href="#home">Navbar</Navbar.Brand>
